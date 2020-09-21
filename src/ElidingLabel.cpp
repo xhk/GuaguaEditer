7255ae4e09f67d7ae13d5e1b2c923c3bd7418854
xq <cangsangbujin@126.com>
2020-09-21 14:37:39 +0800

    complie fix

--- src/ElidingLabel.cpp
+++ src/ElidingLabel.cpp
@@ -66,10 +66,10 @@ void ElidingLabelPrivate::elideText(int Width)
 	}
     QFontMetrics fm = _this->fontMetrics();
     QString  str = fm.elidedText(Text, ElideMode, Width - _this->margin() * 2 - _this->indent());
-    if (str == "…")
+    /*if (str == "…")
     {
     	str = Text.at(0);
-    }
+    }*/
     bool WasElided = IsElided;
     IsElided = str != Text;
     if(IsElided != WasElided)
@@ -169,7 +169,7 @@ QSize CElidingLabel::minimumSizeHint() const
     }
     const QFontMetrics  &fm = fontMetrics();
     #if (QT_VERSION >= QT_VERSION_CHECK(5, 11, 0))
-        QSize size(fm.horizontalAdvance(d->Text.left(2) + "…"), fm.height());
+        QSize size(fm.horizontalAdvance(d->Text.left(2) /*+ "…"*/), fm.height());
     #else
         QSize size(fm.width(d->Text.left(2) + "…"), fm.height());
     #endif
